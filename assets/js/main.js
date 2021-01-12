839b800e93e32b3a608b8821cf084988e0da3fa2
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-12 17:35:47 +0100

    prima milestone completata

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,47 @@ let app = new Vue({
     el: '#app',
     data:{
         moviesArray:[],
-        inputVal: null,
+        searchMovie: '',
     },
     methods:{
-
+        searchFilms(){
+            var config = {
+                method: 'get',
+                url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchMovie}&include_adult=false`,
+                headers: { }
+              };
+              
+              axios(config)
+              .then(response=> {
+                console.log(response.data.results);
+                this.moviesArray = response.data.results;
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }
     },
-    mounted(){
-        var config = {
-            method: 'get',
-            url: 'https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=en-US&query=all&include_adult=false',
-            headers: { }
-          };
+    // computed:{
+    //     filteredMovies(){
+    //         return this.moviesArray.filter(item=>{
+    //             return item.original_title.toLowerCase().includes(this.searchMovie.toLowerCase())
+    //         })
+    //     }
+    // },
+    // mounted(){
+    //     var config = {
+    //         method: 'get',
+    //         url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchMovie}&include_adult=false`,
+    //         headers: { }
+    //       };
           
-          axios(config)
-          .then(response=> {
-            console.log(response.data.results);
-            this.moviesArray = response.data.results;
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
+    //       axios(config)
+    //       .then(response=> {
+    //         console.log(response.data.results);
+    //         this.moviesArray = response.data.results;
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+    // }
 });
