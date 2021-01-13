79c315d5ab3e2c18e01506105a8f450cb0f7fb57
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-13 17:00:26 +0100

    milestone 2 completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,31 +2,87 @@ let app = new Vue({
     el: '#app',
     data:{
         moviesArray:[],
-        searchMovie: '',
+        tvShows: [],
+        searchUser: '',
+
     },
     methods:{
       // chiamata axios per i film
         searchFilms(){
             var config = {
                 method: 'get',
-                url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchMovie}&include_adult=false`,
+                url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchUser}&include_adult=false`,
                 headers: { }
               };
               
               axios(config)
               .then(response=> {
-                console.log(response.data.results);
                 this.moviesArray = response.data.results;
                 console.log(this.moviesArray);
+                this.moviesArray.forEach(movie => {
+                  let rating = Math.ceil(movie.vote_average / 2)
+                  movie.vote_average = rating;
+                  if (movie.original_language == "en"){
+                    movie.original_language = "gb";
+                  } else if (movie.original_language == "zh") {
+                    movie.original_language = "cn"
+                  } else if (movie.original_language == "ko") {
+                    movie.original_language = "kr"
+                  } else if(movie.original_language == "vi"){
+                    movie.original_language = "vn";
+                  }else if(movie.original_language == "et"){
+                    movie.original_language = "ee";
+                  }else if(movie.original_language == "ja"){
+                    movie.original_language = "jp";
+                  }else if(movie.original_language == "da"){
+                    movie.original_language = "dk";
+                  } else if(movie.original_language == "hu"){
+                    movie.original_language = "ua";
+                  }
+                });
               })
+               // error signal
+               .catch(function (error) {
+                console.log(error);
+              });
+              var element = {
+                method: 'get',
+                url: `https://api.themoviedb.org/3/search/tv?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchUser}&include_adult=false`,
+                header: {}
+              }
+              axios(element)
+              .then(response=>{
+                this.tvShows = response.data.results;
+                this.tvShows.forEach(serie => {
+                  let rating = Math.ceil(serie.vote_average / 2)
+                  serie.vote_average = rating;
+                  if (serie.original_language == "en"){
+                    serie.original_language = "gb";
+                  } else if (serie.original_language == "zh") {
+                    serie.original_language = "cn"
+                  } else if (serie.original_language == "ko") {
+                    serie.original_language = "kr"
+                  } else if(serie.original_language == "vi"){
+                    serie.original_language = "vn";
+                  }else if(serie.original_language == "et"){
+                    serie.original_language = "ee";
+                  }else if(serie.original_language == "ja"){
+                    serie.original_language = "jp";
+                  }else if(serie.original_language == "da"){
+                    serie.original_language = "dk";
+                  } else if(serie.original_language == "hu"){
+                    serie.original_language = "ua";
+                  }
+                });
+                console.log(this.tvShows);
+              })
+              // error signal
               .catch(function (error) {
                 console.log(error);
               });
+             
         },
         // cambio di valutazione rating
-        numberCeil(integer){
-           return Math.ceil(((integer - 0) / (10 - 0)) * (5 - 0) + 0)
-        },
         
     },
 });
