bb636af4d12558a95eea5cc7236a185319772e22
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-19 09:32:32 +0100

    refactoring
    miglioramento codice e aggiunta overview in inglese nel caso manchi in italiano

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,41 @@ let app = new Vue({
     el: '#app',
     data:{
         moviesArray:[],
+        moviesArray2: [],
         tvShows: [],
         searchUser: '',
-        errorMsg: null,
 
     },
     methods:{
+
+      // metodo per le bandiere richiesta 
+      flagsMethod(element){
+        if (element.original_language == "en"){
+          element.original_language = "gb";
+        } else if (element.original_language == "zh") {
+          element.original_language = "cn"
+        } else if (element.original_language == "ko") {
+          element.original_language = "kr"
+        } else if(element.original_language == "vi"){
+          element.original_language = "vn";
+        }else if(element.original_language == "ja"){
+          element.original_language = "jp";
+        }else if(element.original_language == "et"){
+          element.original_language = "ee";
+        } else if(element.original_language == "hu"){
+          element.original_language = "ua";
+        }else if(element.original_language == "da"){
+          element.original_language = "dk";
+        }
+      },
       // chiamata axios per i film
         searchFilms(){
+          // chiamata italiano
             var config = {
                 method: 'get',
                 url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchUser}&include_adult=false`,
                 headers: { }
               };
-              
               axios(config)
               .then(response=> {
                 this.moviesArray = response.data.results;
@@ -26,29 +47,25 @@ let app = new Vue({
                   let rating = Math.ceil(movie.vote_average / 2)
                   movie.vote_average = rating;
                   // if per le bandiere sbagliate nella richiesta api
-                  if (movie.original_language == "en"){
-                    movie.original_language = "gb";
-                  } else if (movie.original_language == "zh") {
-                    movie.original_language = "cn"
-                  } else if (movie.original_language == "ko") {
-                    movie.original_language = "kr"
-                  } else if(movie.original_language == "vi"){
-                    movie.original_language = "vn";
-                  }else if(movie.original_language == "ja"){
-                    movie.original_language = "jp";
-                  }else if(movie.original_language == "et"){
-                    movie.original_language = "ee";
-                  } else if(movie.original_language == "hu"){
-                    movie.original_language = "ua";
-                  }else if(movie.original_language == "da"){
-                    movie.original_language = "dk";
-                  }
+                  this.flagsMethod(movie);
                 });
               })
                // error signal
                .catch(function (error) {
                 console.log(error);
               });
+              // !attenzion: seconda chiamata axios per lingua inglese se manca trama in italiano
+              var config = {
+                method: 'get',
+                url: `https://api.themoviedb.org/3/search/movie?api_key=178b153a9c399a44c2973d28bcf08244&language=en-US&query=${this.searchUser}&include_adult=false`,
+                headers: { }
+              };
+              axios(config)
+              .then (response=>{
+                this.moviesArray2 = response.data.results;
+              })
+              //  //
+              
               var config = {
                 method: 'get',
                 url: `https://api.themoviedb.org/3/search/tv?api_key=178b153a9c399a44c2973d28bcf08244&language=it-IT&query=${this.searchUser}&include_adult=true`,
@@ -61,23 +78,8 @@ let app = new Vue({
                   let rating = Math.ceil(serie.vote_average / 2)
                   // if per le bandiere sbagliate nella richiesta api
                   serie.vote_average = rating;
-                  if (serie.original_language == "en"){
-                    serie.original_language = "gb";
-                  } else if (serie.original_language == "zh") {
-                    serie.original_language = "cn"
-                  } else if (serie.original_language == "ko") {
-                    serie.original_language = "kr"
-                  } else if(serie.original_language == "vi"){
-                    serie.original_language = "vn";
-                  }else if(serie.original_language == "et"){
-                    serie.original_language = "ee";
-                  }else if(serie.original_language == "ja"){
-                    serie.original_language = "jp";
-                  }else if(serie.original_language == "da"){
-                    serie.original_language = "dk";
-                  } else if(serie.original_language == "hu"){
-                    serie.original_language = "ua";
-                  }
+                  this.flagsMethod(serie);
+                  
                 });
                 console.log(this.tvShows);
               })
